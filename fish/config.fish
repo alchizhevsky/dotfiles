c3626c7ea60766fd91cbbd276a95bc870652894b
alex <alchizhevsky@gmail.com>
2026-03-06 19:39:57 -0500

    abbr + hotkeys

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+abbr -a vim nvim
+abbr -a hconf nvim ~/.config/hypr/hyprland.conf
+abbr -a hpconf nvim ~/.config/hyprpanel/config.json
+abbr -a fconf nvim ~/.config/fish/config.fish
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
