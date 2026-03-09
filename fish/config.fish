ca732af0f7d1702f025b20d4f6922359aa0af1cc
alex <alchizhevsky@gmail.com>
2026-03-09 03:27:39 +0300

    idk, seems good

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,14 @@
 abbr -a vim nvim
+abbr -a cat bat
+abbr -a curl curlie
+abbr -a ls eza
+abbr -a wpconf nvim ~/.config/hypr/hyprpaper.conf
 abbr -a hconf nvim ~/.config/hypr/hyprland.conf
+abbr -a hlconf nvim ~/.config/hypr/hyprlock.conf
+abbr -a hiconf nvim ~/.config/hypr/hypridle.conf
 abbr -a hpconf nvim ~/.config/hyprpanel/config.json
 abbr -a fconf nvim ~/.config/fish/config.fish
+set -g fish_key_bindings fish_vi_key_bindings
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
