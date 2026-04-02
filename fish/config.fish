0d000c0942f183ef2dc2409cbc936f2c140eba2d
alex <alchizhevsky@gmail.com>
2026-04-02 04:06:40 +0300

    font, cursor, gtk theme, screenchots, haruna, walker

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,11 @@ abbr -a hlconf nvim ~/.config/hypr/hyprlock.conf
 abbr -a hiconf nvim ~/.config/hypr/hypridle.conf
 abbr -a hpconf nvim ~/.config/hyprpanel/config.json
 abbr -a fconf nvim ~/.config/fish/config.fish
+abbr -a gconf nvim ~/.config/ghostty/config
 set -g fish_key_bindings fish_vi_key_bindings
+set -gx XCURSOR_THEME "WhiteSur-cursors"
+set -gx XCURSOR_SIZE 24
+set -gx GTK_FONT_NAME "Inter 11"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
