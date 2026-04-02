ffd9612920c70e7dd8520ae0d90ad4fe0b83028a
alex <alchizhevsky@gmail.com>
2026-04-02 04:29:20 +0300

    themes + dus abbr

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@ abbr -a vim nvim
 abbr -a cat bat
 abbr -a curl curlie
 abbr -a ls eza
+abbr -a dus 'du -ah . | sort -rh | head -n 20'
+
 abbr -a wpconf nvim ~/.config/hypr/hyprpaper.conf
 abbr -a hconf nvim ~/.config/hypr/hyprland.conf
 abbr -a hlconf nvim ~/.config/hypr/hyprlock.conf
@@ -9,6 +11,7 @@ abbr -a hiconf nvim ~/.config/hypr/hypridle.conf
 abbr -a hpconf nvim ~/.config/hyprpanel/config.json
 abbr -a fconf nvim ~/.config/fish/config.fish
 abbr -a gconf nvim ~/.config/ghostty/config
+
 set -g fish_key_bindings fish_vi_key_bindings
 set -gx XCURSOR_THEME "WhiteSur-cursors"
 set -gx XCURSOR_SIZE 24
